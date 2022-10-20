17823f814a93ac3dc92a161ed2f81d6fd252b532
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2022-10-20 06:25:01 +0100

    0.2

--- install.py
+++ install.py
@@ -11,6 +11,7 @@ def Download(url, path):
 def Unzip(filename, extract_dir):
     shutil.unpack_archive(filename, extract_dir)
 
+#3.9
 def Install__3_9__1_7():
     ItemsToDownload = [
         ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_4551993481b847f9a454a382c3c74832.zip", "main.zip", "main/"],
@@ -29,7 +30,26 @@ def Install__3_9__1_7():
             os.remove(item[1])
             print("Cleaned Up", item[1])
 
+def Install__3_9__1_7_2():
+    ItemsToDownload = [
+        ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_484e30e56a0b479f8d0fb4f38e01cb5b.zip", "main.zip", "main/"],
+        ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_ef5aa4fbbc2c4d288a5997aacfb98a15.zip", "pygame.zip", "main/"],
+        ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_f0766125bcb4484f9d2355cbf749cc20.zip", "requests.zip", "main/"],
+        ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_aa3ba115d1a2405abff1a6d1726774fe.zip", "paperclip.zip", "main/"]
+        ]
 
+    for item in ItemsToDownload:
+        Download(item[0], item[1])
+        Unzip(item[1], item[2])
+        print("Downloaded & Extracted", item[1])
+
+    for item in ItemsToDownload:
+        if os.path.exists(item[1]):
+            os.remove(item[1])
+            print("Cleaned Up", item[1])
+
+
+#3.10
 def Install__3_10__1_7():
     ItemsToDownload = [
         ["https://22d78da4-551e-4018-a788-e9419ccd237e.usrfiles.com/archives/22d78d_3a582cdbede545fa801433c54f3d43c2.zip", "main.zip", "main/"],
